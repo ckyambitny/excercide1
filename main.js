c7d71f1d3aadc54d571845832655cb1e74240cb6
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-05-30 11:31:26 +0200

    second

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('ldkaksda');
+var Calculator = function(a) {
+    var operationList = {
+        add: function (y) {
+            a += y;
+            return operationList;
+        },
+        multiply: function (z){
+            a *= z;
+            return this;
+        },
+        value: function () {
+            return a;
+        }
+    }
+        
+      
+  
+    return operationList; 
+
+    
+};
+
+
+ 
+
